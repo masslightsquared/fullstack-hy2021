2a4fe968c044b7b0bdcdf57c535fd6aa8fb85e24
masslightsquared <34934426+masslightsquared@users.noreply.github.com>
2021-10-24 14:58:09 -0400

    part1 assignments

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(new Array(anecdotes.length).fill(0))
+
+  const handleQuote = () => 
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+
+  const handleVote = () => {
+    const votes = [...vote]
+    votes[selected] += 1
+    setVote(votes) 
+  }
+
+  const returnMax = () => {
+    let max = 0
+    let indexMax = 0
+    vote.forEach((item, index) => {
+      if (item > max) {
+        max = item
+        indexMax = index
+      }
+    })
+    return indexMax
+  }
+  
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <table>
+        <tbody>
+        <tr>
+        <td>{anecdotes[selected]}</td>
+        </tr>
+        <td>has {vote[selected]} votes</td>
+        </tbody>
+      </table>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={handleQuote}>Get Anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      {vote[returnMax()] === 0 ? 
+        <>
+          <p>No votes recorded</p>
+        </>
+        :
+        <div>
+        <table>
+          <tbody>
+            <tr>
+              <td>{anecdotes[returnMax()]}</td>
+            </tr>
+            <td>has {vote[returnMax()]} votes</td>
+          </tbody>
+        </table>
+          <p></p>
+          <p></p>
+        </div>
+      }
+    </div>
+  )
+}
+
+export default App
