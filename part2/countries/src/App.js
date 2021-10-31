32e3359d914f289fdad98279a2ebed7e4cf4ba4c
masslightsquared <34934426+masslightsquared@users.noreply.github.com>
2021-10-31 09:23:24 -0400

    fullstack2021 update

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,7 +3,10 @@ import React, { useState, useEffect } from 'react'
 
 
 const App = () => {
+  const [ country, setCountry ] = useState("")
   const [ countries, setCountries ] = useState([])
+  const [ countriesFilter, setCountriesFilter ] = useState([])
+  const [ showCountry, setShowCountry ] = useState({})
 
   useEffect(() => {
     console.log('effect')
@@ -15,11 +18,44 @@ const App = () => {
     })
   }, [])
 
-  return (
-    countries.map((country) => (
+  useEffect(() => {
+    setShowCountry(
+      countriesFilter.length === 1 ? { ...countriesFilter[0] } : {}
+    )
+  }, [countriesFilter])
+
+  const searchCountry = (e) => {
+    setCountry(e.target.value)
+    setCountriesFilter(
+      countries.filter(
+        (country) =>
+        country.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+      )
+    )
+  }
+
+  const showCountries = () => {
+    return countriesFilter.map((country) => (
       <p key={country.name}>
         {country.name}
+        <button onClick={() => setShowCountry(country)}>show</button>
       </p>
     ))
+  }
+
+  return (
+    <>
+      <p>
+        find countries{" "}
+        <input value={country} onChange={searchCountry} />
+      </p>
+      {countriesFilter.length > 10 ? (
+        <p>Too many matches, specify another filter</p>
+      ) : (
+        showCountries()
+      )}
+    </>
+  )
+}
 
 export default App
